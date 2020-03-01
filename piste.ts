7bad85b270205079d310e2f48632da2a8944ad6b
Doug Patti <douglas.patti@gmail.com>
2020-03-01 11:49:05 -0500

    add types to parser

--- piste.ts
+++ piste.ts
@@ -1,9 +1,11 @@
 const split = (arr, n) => [arr.slice(0, n), arr.slice(n)]
 
+type Computation = { alias: string, f: (...Expr) => Expr }
 const Computation = (alias, f) => ({ alias, f });
 Computation.equal = (x, y) => x.alias === y.alias;
 Computation.toString = (x) => x.alias;
 
+type Expr = { comp: Computation, args: Expr[] }
 const Expr = (comp, args=[]) => ({ comp, args })
 Expr.equal = (x, y) =>
   x.comp == y.comp
@@ -69,28 +71,35 @@ Expr.toString = (expr) => {
 
 Expr.parse = (str) => {
   // Obviously first we write a parser combinator library
-  const _ = (a, str) => [a, str];
-  const pure = (a) => (str) => [_(a, str)];
-  const fail = [];
-  const map = (t, f) => (str) => t(str).map(([a, str]) => _(f(a), str));
-  const bind = (t, f) => (str) => t(str).flatMap(([a, str]) => f(a)(str));
-  const alt = (...ts) => (str) => ts.flatMap(t => t(str));
-  const char = (c) => (str) => str[0] == c ? [_(str[0], str.slice(1))] : fail;
-  const seq = (...ts) =>
+  type Parser<a> = (_ : string) => [a, string][];
+  // Tuple constructor
+  const _ = <a>(a: a, str: string): [a, string] => [a, str];
+  const pure = <a>(a: a): Parser<a> => (str) => [_(a, str)];
+  const fail: Parser<any> = (str: string) => [];
+  const map = <a, b>(t: Parser<a>, f: (a: a) => b): Parser<b> =>
+    (str) => t(str).map(([a, str]) => _(f(a), str));
+  const bind = <a, b>(t: Parser<a>, f: (a: a) => Parser<b>): Parser<b> =>
+    (str) => t(str).flatMap(([a, str]) => f(a)(str));
+  const ignore = <a>(t: Parser<a>): Parser<any> => map(t, _ => undefined);
+  const alt = <a>(...ts: Parser<a>[]): Parser<a> =>
+    (str) => ts.flatMap(t => t(str));
+  const seq = <a>(...ts: Parser<a>[]): Parser<a[]> =>
     (ts.length === 0)
       ? pure([])
       : bind(ts[0], x => map(seq(...ts.slice(1)), rest => [x, ...rest]));
-  const many = (t) => {
-    const more = bind(t, a => map(many(t), rest => [a, ...rest]));
-    const done = pure([]);
+  const many = <a>(t: Parser<a>): Parser<a[]> => {
+    const more: Parser<a[]> = bind(t, a => map(many(t), rest => [a, ...rest]));
+    const done: Parser<a[]> = pure([]);
 
     return alt(more, done);
   };
-  const fix = (f) => {
+  const fix = <a>(f: (_: Parser<a>) => Parser<a>) => {
     let t = (str) => f(t)(str);
     return t
   };
-  const run = (t, str) => {
+  const char = (c: string): Parser<string> =>
+    (str) => str[0] == c ? [_(str[0], str.slice(1))] : [];
+  const run = <a>(t: Parser<a>, str: string): a => {
     for (let [a, unconsumed] of t(str)) {
       if (unconsumed === '') return a;
     }
@@ -100,20 +109,26 @@ Expr.parse = (str) => {
   }
 
   // Okay. There. That wasn't so hard now was it
-  const symbol =
+  const symbol: Parser<Expr> =
     alt(
       map(char('S'), _ => Expr(S)),
       map(char('K'), _ => Expr(K)),
       map(char('I'), _ => Expr(I))
     );
 
-  const group = (p) =>
-    map(seq(char('('), p, char(')')), ([_l, ps, _r]) => ps);
+  const group = (p: Parser<Expr>): Parser<Expr> =>
+    map(
+      seq(
+        ignore(char('(')),
+        p,
+        ignore(char(')'))
+      ),
+      ([_l, ps, _r]) => ps);
 
-  const expr =
+  const expr: Parser<Expr> =
     fix(expr =>
-      map(many(alt(symbol, group(expr))), exprs =>
-        (exprs.length > 0) ? squash(exprs) : fail));
+      bind(many(alt(symbol, group(expr))), exprs =>
+        (exprs.length > 0) ? pure(squash(exprs)) : fail));
 
   return run(expr, str);
 };
